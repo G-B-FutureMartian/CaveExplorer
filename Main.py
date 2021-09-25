06fdf3d25c0f522f1d56d8f5fb7c2e818f4009bb
G-B-FutureMartian <81188293+G-B-FutureMartian@users.noreply.github.com>
2021-09-25 17:37:05 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,6 @@
 from random import choice, randint
+
+print("Welcome to cave explorer, a MUSH exploration game."
+client = input("Client or host?")
+if client === "Host" or "host":
+      from flask import App, render_template
