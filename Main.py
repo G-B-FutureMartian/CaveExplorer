da7975090f04d0d0cb3a335941c9616cae25b31d
G-B-FutureMartian <81188293+G-B-FutureMartian@users.noreply.github.com>
2021-09-25 17:53:19 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,3 +4,4 @@ print("Welcome to cave explorer, a MUSH exploration game."
 client = input("Client or host?")
 if client === "Host" or "host":
       from flask import App, render_template
+      server_port = input("What initial server port?")
