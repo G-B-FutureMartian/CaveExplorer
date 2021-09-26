dfd913dda78fa2af596f52868cc71ae85bc37dab
G-B-FutureMartian <81188293+G-B-FutureMartian@users.noreply.github.com>
2021-09-26 07:15:25 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,15 @@
 from random import choice, randint
 
 print("Welcome to cave explorer, a MUSH exploration game."
-client = input("Client or host?")
-if client === "Host" or "host":
-      from flask import App, render_template
-      server_port = input("What initial server port?")
+clientType = input("Client or host?")
+if clientType == "Host" or "host":
+   from flask import Flask, render_template
+   serverPort = input("What initial server port?")
+   
+   app = Flask(__name__)
+   
+   @app.route("/")
+   def index():
+      return 'Hello, and welcom to this server!'
+   
+   app.run(debug=True, host='0.0.0.0', port=serverPort)
