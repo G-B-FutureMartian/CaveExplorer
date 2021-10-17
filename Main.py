67a6cb0e03f97de985eff519f41155c284cd2351
G-B-FutureMartian <81188293+G-B-FutureMartian@users.noreply.github.com>
2021-10-17 08:22:07 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 import sys
-import singleplayer.py
+import singleplayer
 
 print("Welcome to cave explorer, a MUSH exploration game."
 user = input("How do you want to play? SinglePlayer(BETA) or MultiPlayer?")
+if user == "SinglePlayer" or "singlePlayer":
+      main()
