ea338f1ec21e020192e857281a1861309ce847d6
G-B-FutureMartian <81188293+G-B-FutureMartian@users.noreply.github.com>
2021-10-17 08:15:21 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,5 @@
-from random import choice, randint
+import sys
+import singleplayer.py
 
 print("Welcome to cave explorer, a MUSH exploration game."
-clientType = input("Client or host?")
-if clientType == "Host" or "host":
-   from flask import Flask, render_template
-   serverPort = input("What initial server port?")
-   
-   app = Flask(__name__)
-   
-   @app.route("/")
-   def index():
-      return 'Hello, and welcom to this server!'
-   
-   app.run(debug=True, host='0.0.0.0', port=serverPort)
+user = input("How do you want to play? SinglePlayer(BETA) or MultiPlayer?")
