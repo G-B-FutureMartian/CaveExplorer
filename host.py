5b757c2b591dcef4f8e6bcc8043810354a1f767c
G-B-FutureMarti <81188293+G-B-FutureMartian@users.noreply.github.com>
2022-01-06 14:35:29 +0000

    Host function added, so you could host a server.

--- host.py
+++ host.py
@@ -1,4 +1,13 @@
 from flask import Flask
+from time import sleep
+app = Flask(__name__)
+
+def host(route, userPort):
+  @app.route(route)
+  def index():
+    return("Hello, World")
+  sleep(3)
+  app.run(debug=False, host="0.0.0.0", port=userPort)
 
 if __name__ == "__main__":
   print("You will now begin hosting an empty room.")
