f1c646674cf08b5492fd55014424144235260d67
G-B-FutureMarti <81188293+G-B-FutureMartian@users.noreply.github.com>
2022-01-06 14:17:00 +0000

    Fix .replit and create a file for hosting.

---
+++ host.py
@@ -0,0 +1,7 @@
+from flask import Flask
+
+if __name__ == "__main__":
+  print("You will now begin hosting an empty room.")
+  print("You will be using the Flask framework for a local server.")
+  if input("Ok? ") == "Yes" or "yes":
+    print("Good.")
