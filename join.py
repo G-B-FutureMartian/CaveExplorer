40fca08b04a486f06d8aedd54ceebf002fbd8679
G-B-FutureMartian <astronomonster1969@gmail.com>
2023-09-09 17:12:11 -0400

    Start programming join.py

---
+++ join.py
@@ -0,0 +1,2 @@
+def joinGameStream(ip, userPort=8000):
+    url = 'http://{0}:{1}'.format(ip,userPort)
--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import singleplayer
 from host import host, setupHosting
-import join
+from join import joinGameStream
 
 def multiUser():
-
-
   yNo = input("Would you like to host? ")
   if 'y' or 'Y' in yNo:
     userPort,route = setupHosting()
     host(route, userPort)
   elif 'n' in yNo:
-
-  
+    port = input("What port do you need?")
+    joinGameStream(port.int())
     print("Nothing here right now.")
 
 print("Welcome to cave explorer, a MUSH game.")
