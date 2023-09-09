a0847131dbf2e907c20a198e5b50cfc992533dea
G-B-FutureMartian <astronomonster1969@gmail.com>
2023-09-09 17:44:01 -0400

    Fix broken code

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from join import joinGameStream
 
 def multiUser():
   yNo = input("Would you like to host? ")
-  if 'y' or 'Y' in yNo:
+  if 'y' in yNo:
     userPort,route = setupHosting()
     host(route, userPort)
   elif 'n' in yNo:
     port = input("What port do you need?")
-    joinGameStream(port.int())
-    print("Nothing here right now.")
+    joinGameStream(int(port))
+    #print("Nothing here right now.")
 
 print("Welcome to cave explorer, a MUSH game.")
 users = input("Would you like to play Multiplayer?")
-if "y" in users:
+if 'y' in users:
     multiUser() 
 else:
-    pass
+    print('Perfecto')
